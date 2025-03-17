8bcdd55657c034f5097857115bed467a351fbdcd
ezio <ezio.xie@binance.com>
2025-03-17 14:05:12 +0000

    table

--- src/schema/twitter-posts.ts
+++ src/schema/twitter-posts.ts
@@ -5,6 +5,8 @@ export const twitterPosts = sqliteTable('twitter_posts', {
   id: integer('id').primaryKey({ autoIncrement: true }),
   url: text('url').notNull(),
   createdAt: integer('created_at', { mode: 'timestamp' })
-    .default(sql`CURRENT_TIMESTAMP`)
-    .notNull(),
+    .default(sql`CURRENT_TIMESTAMP`),
+  lastChecked: integer('last_checked', { mode: 'timestamp' })
+    .default(sql`CURRENT_TIMESTAMP`),
+  status: integer('status', { mode: 'boolean' }).default(true),
 });
